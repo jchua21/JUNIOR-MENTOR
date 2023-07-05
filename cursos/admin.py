1c4a12db504e2355b57760bdb10ef8c60b73f472
Fluchi <fcardenasm@unsa.edu.pe>
2023-07-04 19:22:22 -0500

    mejoras funcionales

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Curso, Comment
 
-# Register your models here.
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'categories', 'user',
+                    'location', 'price', 'taste', 'persons', ]
+    search_fields = ['title']
+    list_filter = ['user', 'created_at']
+    list_per_page = 20
+    date_hierarchy = 'created_at'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'user', 'created_at']
+    search_fields = ['text']
+    list_per_page = 20
+    date_hierarchy = 'created_at'
--- cursos/forms.py
+++ cursos/forms.py
@@ -1,21 +1,18 @@
 from django import forms
-from .models import Curso,Comment
+from .models import Curso, Comment
 from .snippets import choices
 
 class CursoCreateForm(forms.ModelForm):
-    title = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder': 'Enter title'}))
-    categories = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Categories separated by comma. Example: chinese,thai'}))
+    title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese el título'}))
+    categories = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Categorías separadas por coma. Ejemplo: chino, tailandés'}))
     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
-    location = forms.CharField(widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Enter Location'}))
-    price = forms.IntegerField(widget=forms.NumberInput(
-        attrs={'class': 'form-control', 'placeholder': 'Enter Price'}))
-    vat = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control','placeholder':'Vat in %'}))
-    taste = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}),choices=choices)
-    persons = forms.ChoiceField(widget=forms.Select(
-        attrs={'class': 'form-control'}), choices=choices)
+    location = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese la ubicación'}))
+    price = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese el precio'}))
+    taste = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), choices=choices)
+    persons = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), choices=choices)
     details = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Curso
-        fields = ['title','image','categories','location','price','vat','taste','persons','details']
+        fields = ['title', 'image', 'categories', 'location', 'price', 'taste', 'persons', 'details']
+
