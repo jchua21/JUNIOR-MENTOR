e316b0b03c306d9cf16ed1be9222409d42fc4964
jchua21 <jchua@unsa.edu.pe>
2023-08-01 12:09:58 -0500

    Rutas 2

--- JuniorMentor/settings.py
+++ JuniorMentor/settings.py
@@ -139,7 +139,7 @@ if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
      STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+     # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
